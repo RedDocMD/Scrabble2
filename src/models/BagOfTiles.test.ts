e5f1e67a913be589fea5daaccefdeee2ae3dcf50
Deep Majumder <deep.majumder2019@gmail.com>
2020-11-20 20:31:28 +0530

    Added BagOfTiles class and its test file

---
+++ src/models/BagOfTiles.test.ts
@@ -0,0 +1,42 @@
+import BagOfTiles from './BagOfTiles';
+import Tile from './Tile';
+
+test('Initially bag has 100 tiles', () => {
+    let bag = new BagOfTiles(false);
+    expect(bag.tileCount).toBe(100);
+});
+
+test('Draw 7 tiles, then 5, then 1', () => {
+    let bag = new BagOfTiles(false);
+    let tiles: Tile[];
+
+    [tiles, bag] = bag.drawTiles(7);
+    expect(tiles.length).toBe(7);
+    expect(bag.tileCount).toBe(93);
+
+    [tiles, bag] = bag.drawTiles(5);
+    expect(tiles.length).toBe(5);
+    expect(bag.tileCount).toBe(88);
+
+    [tiles, bag] = bag.drawTiles(1);
+    expect(tiles.length).toBe(1);
+    expect(bag.tileCount).toBe(87);
+})
+
+test('Draw negative tiles', () => {
+    let bag = new BagOfTiles(false);
+    expect(() => {
+        bag.drawTiles(-1);
+    }).toThrow('can draw between 1 and 7 tiles (both inclusive)');
+})
+
+test('Overdraw tiles', () => {
+    let bag = new BagOfTiles(false);
+    let tiles: Tile[];
+    for (let i = 0; i < 14; i++) {
+        [tiles, bag] = bag.drawTiles(7);
+    }
+    expect(() => {
+        bag.drawTiles(5);
+    }).toThrow('insufficient tiles');
+})
---
+++ src/models/BagOfTiles.ts
@@ -0,0 +1,188 @@
+import Tile from "./Tile";
+import shuffle from 'shuffle-array';
+
+export default class BagOfTiles {
+    private tiles: Tile[];
+
+    constructor(empty: boolean) {
+        this.tiles = [];
+        if (!empty) {
+            for (let info of initialBagInfo) {
+                for (let i = 0; i < info.count; i++) {
+                    let tile = new Tile(info.point, info.letter);
+                    this.tiles.push(tile);
+                }
+            }
+        }
+    }
+
+    private fromTiles(tiles: Tile[]) {
+        let bag = new BagOfTiles(false);
+        bag.tiles = tiles;
+        return bag;
+    }
+
+    get tileCount() {
+        return this.tiles.length;
+    }
+
+    drawTiles(count: number): [Tile[], BagOfTiles] {
+        if (count < 1 || count > 7) {
+            throw new Error('can draw between 1 and 7 tiles (both inclusive)');
+        }
+        if (count > this.tiles.length) {
+            throw new Error('insufficient tiles');
+        }
+        let shuffledTiles = shuffle(this.tiles);
+        let returnedTiles = shuffledTiles.slice(-count);
+        let remainingTiles = shuffledTiles.slice(0, shuffledTiles.length - count);
+        return [
+            returnedTiles,
+            this.fromTiles(remainingTiles)
+        ]
+    }
+}
+
+interface TileInfo {
+    letter: string,
+    point: number,
+    count: number
+}
+
+const initialBagInfo: TileInfo[] = [
+    {
+        letter: 'A',
+        point: 1,
+        count: 9
+    },
+    {
+        letter: 'B',
+        point: 3,
+        count: 2
+    },
+    {
+        letter: 'C',
+        point: 3,
+        count: 2
+    },
+    {
+        letter: 'D',
+        point: 2,
+        count: 4
+    },
+    {
+        letter: 'E',
+        point: 1,
+        count: 12
+    },
+    {
+        letter: 'F',
+        point: 4,
+        count: 2
+    },
+    {
+        letter: 'G',
+        point: 2,
+        count: 3
+    },
+    {
+        letter: 'H',
+        point: 4,
+        count: 2
+    },
+    {
+        letter: 'I',
+        point: 1,
+        count: 9
+    },
+    {
+        letter: 'J',
+        point: 8,
+        count: 1
+    },
+    {
+        letter: 'K',
+        point: 5,
+        count: 1
+    },
+    {
+        letter: 'L',
+        point: 1,
+        count: 4
+    },
+    {
+        letter: 'M',
+        point: 3,
+        count: 2,
+    },
+    {
+        letter: 'N',
+        point: 1,
+        count: 6
+    },
+    {
+        letter: 'O',
+        point: 1,
+        count: 8
+    },
+    {
+        letter: 'P',
+        point: 3,
+        count: 2
+    },
+    {
+        letter: 'Q',
+        point: 10,
+        count: 1
+    },
+    {
+        letter: 'R',
+        point: 1,
+        count: 6
+    },
+    {
+        letter: 'S',
+        point: 1,
+        count: 4
+    },
+    {
+        letter: 'T',
+        point: 1,
+        count: 6
+    },
+    {
+        letter: 'U',
+        point: 1,
+        count: 4
+    },
+    {
+        letter: 'V',
+        point: 4,
+        count: 2
+    },
+    {
+        letter: 'W',
+        point: 4,
+        count: 2
+    },
+    {
+        letter: 'X',
+        point: 8,
+        count: 1
+    },
+    {
+        letter: 'Y',
+        point: 4,
+        count: 2
+    },
+    {
+        letter: 'Z',
+        point: 10,
+        count: 1
+    },
+    {
+        letter: ' ',
+        point: 0,
+        count: 2
+    }
+];
